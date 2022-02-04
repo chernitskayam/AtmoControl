d657b537c68454fddf1255ba6d313d131413e84f
chernitskayam <s239827@std.novsu.ru>
2022-02-05 02:31:22 +0300

    'green style with JSON v1'

--- app/src/main/java/com/example/atmocontrol/MainActivity.java
+++ app/src/main/java/com/example/atmocontrol/MainActivity.java
@@ -4,17 +4,88 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String JSON_URL = "http://api.blueberry.gq/info_to_app.php";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        loadJSONFromURL(JSON_URL);
     }
 
-    public void update(View v)
-    {
+    public void update (View v){
+        loadJSONFromURL(JSON_URL);
+    }
 
+    public void loadJSONFromURL(String url){
+        final ProgressBar progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        progressBar.setVisibility(ListView.VISIBLE);
+        TextView tempJSON = findViewById(R.id.tempValue);
+        TextView humJSON = findViewById(R.id.humValue);
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>(){
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject object = new JSONObject(response);
+                            JSONArray jsonArray = object.getJSONArray("data");
+                            ArrayList<JSONObject> listItems = getArrayListFromJSONArray(jsonArray);
+                            try{
+                                tempJSON.setText(listItems.get(0).getString("temp"));
+                                humJSON.setText(listItems.get(0).getString("humidity"));
+                                progressBar.setVisibility(View.INVISIBLE);
+                            }catch (JSONException je){
+                                je.printStackTrace();
+                            }
+                        }catch (JSONException e){
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener(){
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(getApplicationContext(),error.getMessage(),Toast.LENGTH_SHORT).show();
+                    }
+                });
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
+    }
+
+    private ArrayList<JSONObject> getArrayListFromJSONArray(JSONArray jsonArray){
+        ArrayList<JSONObject> aList = new ArrayList<JSONObject>();
+        try {
+            if(jsonArray!= null){
+                for(int i = 0; i<jsonArray.length();i++){
+                    aList.add(jsonArray.getJSONObject(i));
+                }
+            }
+        }catch (JSONException js){
+            js.printStackTrace();
+        }
+        return aList;
     }
 }
